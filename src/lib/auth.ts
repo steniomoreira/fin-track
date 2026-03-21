f4e095cbe794063523855d4a1e66efd87a7475d4
steniomoreira <steniomoreiradev@gmail.com>
2026-03-21 14:46:00 -0300

    chore: add resend e settings auth file

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,10 @@
 import { betterAuth } from 'better-auth';
 import { prismaAdapter } from 'better-auth/adapters/prisma';
+import { Resend } from 'resend';
 
 import { db } from './prisma';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const auth = betterAuth({
   database: prismaAdapter(db, {
@@ -9,7 +12,21 @@ export const auth = betterAuth({
   }),
   emailAndPassword: {
     enabled: true,
+    sendResetPassword: async ({ url }) => {
+      await resend.emails.send({
+        from: 'onboarding@resend.dev', // PROD => no-reply@seudominio.com
+        to: 'steniomoreiradev@gmail.com', // PROD => { user.email }
+        subject: 'Redefinição de senha',
+        html: `
+          <p>Olá,</p>
+          <p>Clique no link abaixo para redefinir sua senha:</p>
+          <a href="${url}">${url}</a>
+          <p>O link expira em 1 hora.</p>
+        `,
+      });
+    },
   },
+
   session: {
     expiresIn: 60 * 60 * 24 * 7, // 7 days
     updateAge: 60 * 60 * 24, // refresh if older than 1 day
