183abad41cd5d64330ebd14829d5c1afc3378468
steniomoreira <steniomoreiradev@gmail.com>
2026-03-22 00:48:32 -0300

    feat: add items navigation in sidebar

--- src/app/(protected)/_components/app-sidebar.tsx
+++ src/app/(protected)/_components/app-sidebar.tsx
@@ -1,24 +1,55 @@
+import { ArrowLeftRight, LayoutDashboard } from 'lucide-react';
+import Link from 'next/link';
+
+import FinTrack from '@/components/logos/fintrack';
 import {
   Sidebar,
   SidebarContent,
   SidebarFooter,
-  SidebarGroup,
   SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
 } from '@/components/ui/sidebar';
 
 import SignOutButton from '../dashboard/_components/sign-out-button';
 
+const items = [
+  {
+    title: 'Dashboard',
+    url: '/dashboard',
+    icon: LayoutDashboard,
+  },
+  {
+    title: 'Trasações',
+    url: '/transactions',
+    icon: ArrowLeftRight,
+  },
+];
+
 export function AppSidebar() {
   return (
     <Sidebar>
-      <SidebarHeader>
-        <SignOutButton />
+      <SidebarHeader className="p-6">
+        <FinTrack />
       </SidebarHeader>
       <SidebarContent>
-        <SidebarGroup />
-        <SidebarGroup />
+        <SidebarMenu>
+          {items.map((item) => (
+            <SidebarMenuItem key={item.title}>
+              <SidebarMenuButton asChild>
+                <Link href={item.url}>
+                  <item.icon />
+                  <span>{item.title}</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          ))}
+        </SidebarMenu>
       </SidebarContent>
-      <SidebarFooter />
+      <SidebarFooter>
+        <SignOutButton />
+      </SidebarFooter>
     </Sidebar>
   );
 }
--- src/app/(public)/sign-in/_component/sign-in-form.tsx
+++ src/app/(public)/sign-in/_component/sign-in-form.tsx
@@ -117,6 +117,7 @@ export function SignInForm({
               <InputGroup>
                 <InputGroupInput
                   {...field}
+                  id="password"
                   type={isShow ? 'text' : 'password'}
                   placeholder="********"
                 />
