27d46e9e1c186b14a6b5fbc21301add3e10e86b0
steniomoreira <steniomoreiradev@gmail.com>
2026-03-29 20:49:23 -0300

    feat: add getInstallmentTransactionById function to retrieve installment transaction details

---
+++ src/actions/transactions/get-installmet-transaction-by-id.ts
@@ -0,0 +1,57 @@
+'use server';
+
+import { db } from '@/lib/prisma';
+import { requireSession } from '@/lib/session';
+
+export async function getInstallmentTransactionById(id: string) {
+  const session = await requireSession();
+
+  try {
+    const installment = await db.installment.findUnique({
+      where: {
+        id,
+        userId: session.user.id,
+      },
+      select: {
+        id: true,
+        dueDate: true,
+        amount: true,
+        status: true,
+        number: true,
+        transaction: {
+          select: {
+            id: true,
+            description: true,
+            type: true,
+            numberInstallments: true,
+            category: {
+              select: {
+                id: true,
+                name: true,
+              },
+            },
+            creditCard: {
+              select: {
+                id: true,
+                name: true,
+                cardNumber: true,
+              },
+            },
+          },
+        },
+        payments: {
+          select: {
+            id: true,
+            date: true,
+            amount: true,
+          },
+        },
+      },
+    });
+
+    return { installment };
+  } catch (error) {
+    console.error(error);
+    return { installment: null };
+  }
+}
