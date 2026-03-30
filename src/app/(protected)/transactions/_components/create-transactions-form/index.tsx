46b7000aff1c7a2b828b971300809a5d875e8090
steniomoreira <steniomoreiradev@gmail.com>
2026-03-29 22:04:53 -0300

    refactor: remove unused Link import from transaction forms

--- src/app/(protected)/transactions/_components/create-transactions-form/index.tsx
+++ src/app/(protected)/transactions/_components/create-transactions-form/index.tsx
@@ -2,7 +2,6 @@
 
 import { zodResolver } from '@hookform/resolvers/zod';
 import { ArrowRight, Calendar, FileText, Loader2 } from 'lucide-react';
-import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { FormProvider, useForm } from 'react-hook-form';
 import { toast } from 'sonner';
